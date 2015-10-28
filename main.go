895db18abb698f41cb6d2d5f8e4c4ef3f0409a7c
Sergey Parshukov <sergey.parshukov@me.com>
2015-10-28 15:42:39 +0300

    Some refactoring

--- main.go
+++ main.go
@@ -49,10 +49,19 @@ func preparePattern(item poe.Item) []float64 {
 	return p
 }
 
-func main() {
+func testNN(filename string, nn gobrain.FeedForward, maxPrice float64) {
+	items, err := poe.LoadFromFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, item := range items {
+		fmt.Println(item)
+		inputs := preparePattern(item)
+		fmt.Println(item.Price, "->", nn.Update(inputs)[0]*maxPrice)
+	}
+}
 
-	fmt.Println("== Items ==")
-	//	items, err := poe.LoadFromFile("data.txt")
+func loadItems() []poe.Item {
 	items, err := poe.LoadFromJsonFile("jewels.json")
 	if err != nil {
 		log.Fatal(err)
@@ -63,15 +72,13 @@ func main() {
 	}
 	items = append(items, items2...)
 	fmt.Println("Items loaded:", len(items))
-	//	for _, it := range items {
-	//		fmt.Println(it)
-	//	}
+	return items
+}
 
-	fmt.Println("== Feature vectors ==")
+func main() {
+
+	items := loadItems()
 	patterns, maxPrice := preparePatterns(items)
-	//	for _, p := range patterns {
-	//		fmt.Println(p)
-	//	}
 
 	fmt.Println("== Training ==")
 	nn := gobrain.FeedForward{}
@@ -81,36 +88,5 @@ func main() {
 	nn.Test(patterns)
 
 	fmt.Println("== Test ==")
-	testFunc := func(text, price string) {
-		item, err := poe.ParseItem(text)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(item)
-		inputs := preparePattern(item)
-		fmt.Println(price, "->", nn.Update(inputs)[0]*maxPrice)
-	}
-
-	testFunc(`Gale Edge Crimson Jewel
-13% increased Physical Weapon Damage while Dual Wielding
-8% increased Attack Speed with Swords
-3% reduced Mana Cost of Skills`, "5 chaos")
-	testFunc(`Blight Glimmer Crimson Jewel
-15% increased Weapon Critical Strike Chance while Dual Wielding
-5% increased maximum Life
-14% increased Freeze Duration on Enemies
-2% chance to Freeze`, "20 chaos")
-	testFunc(`Spirit Splinter Crimson Jewel
-15% increased Fire Damage
-10% increased Critical Strike Multiplier with One Handed Melee Weapons
-4% increased Attack and Cast Speed`, "2 chaos")
-	testFunc(`Chimeric Flame Crimson Jewel
-6% increased Attack Speed while holding a Shield
-0.2% of Physical Attack Damage Leeched as Life
-11% increased Area Damage
-Totems gain +6% to all Elemental Resistances`, "3 chaos")
-	testFunc(`Ghoul Ruin Viridian Jewel
-14% increased Physical Damage with Claws
-13% increased Chaos Damage
-5% increased Attack Speed`, "10 chaos")
+	testNN("test_jewels.txt", nn, maxPrice)
 }
