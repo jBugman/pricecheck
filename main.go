c838b9a9f2aed2efb4dd1ea76bd2d88c849fdda1
Sergey Parshukov <codedby@bugman.me>
2016-12-22 16:28:28 +0300

    Some statistics experiments

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/goml/gobrain"
+	"github.com/montanaflynn/stats"
 
 	"github.com/jBugman/pricecheck/poe"
 )
@@ -76,17 +77,53 @@ func loadItems() []poe.Item {
 }
 
 func main() {
-
 	items := loadItems()
-	patterns, maxPrice := preparePatterns(items)
+	prices := make(map[string][]float64)
+	for _, item := range items {
+		for mod := range item.Params {
+			prices[mod] = append(prices[mod], item.Price)
+		}
+	}
+	for k, p := range prices {
+		min, _ := stats.Min(p)
+		max, _ := stats.Max(p)
+		mean, _ := stats.Mean(p)
+		std, _ := stats.StandardDeviation(p)
+		qo, _ := stats.QuartileOutliers(p)
+		if len(qo.Extreme) > 0 || len(qo.Mild) > 0 {
+			fmt.Printf("%.2f %.2f %.2f %.2f %v %v %d %s\n", min, max, mean, std, qo.Mild, qo.Extreme, len(p), k)
+			ol := make(map[float64]bool)
+			for _, x := range append(qo.Extreme, qo.Mild...) {
+				ol[x] = true
+			}
+			var pr []float64
+			for _, x := range p {
+				if _, ok := ol[x]; !ok {
+					pr = append(pr, x)
+				}
+			}
+			min, _ = stats.Min(pr)
+			max, _ = stats.Max(pr)
+			mean, _ = stats.Mean(pr)
+			std, _ = stats.StandardDeviation(pr)
+			qo, _ = stats.QuartileOutliers(pr)
+			fmt.Printf("Fixed: %.2f %.2f %.2f %.2f %v %v %d\n", min, max, mean, std, qo.Mild, qo.Extreme, len(pr))
+		}
+	}
 
-	fmt.Println("== Training ==")
-	nn := gobrain.FeedForward{}
-	nn.Init(size, int(hiddenLayerK*float64(size)), 1)
-	nn.Train(patterns, iterations, learningRate, mFactor, true)
+//	median, _ := stats.Median(prices)
+//	mean, _ := stats.Mean(prices)
+//	midhinge, _ := stats.Midhinge(prices)
+//	std, _ := stats.StandardDeviation(prices)
+//	fmt.Println("min", min, "max", max, "median", median, "mean", mean, "midhinge", midhinge, "std", std)
 
-	nn.Test(patterns)
+//	patterns, maxPrice := preparePatterns(items)
 
-	fmt.Println("== Test ==")
-	testNN("test_jewels.txt", nn, maxPrice)
+//	fmt.Println("== Training ==")
+//	nn := gobrain.FeedForward{}
+//	nn.Init(size, int(hiddenLayerK*float64(size)), 1)
+//	nn.Train(patterns, iterations, learningRate, mFactor, true)
+////	nn.Test(patterns)
+//	fmt.Println("== Test ==")
+//	testNN("test_jewels.txt", nn, maxPrice)
 }
